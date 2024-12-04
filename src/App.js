7b22a7c6e2ab597d8f323a47aebdc12bfbfcb02a
Samrajnee <samrajnee30@gmail.com>
2024-12-04 12:03:10 +0530

    Header done successfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "../src/components/Header/header.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <Header/>
     </div>
   );
 }
